3c7699d5475792ce08ba6e829fa04320861a8549
Subh2101 <90567811+Subh2101@users.noreply.github.com>
2024-01-31 15:20:49 +0530

    Update hero_home

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -11,8 +11,10 @@ const Hero = () => {
         <div className={styles.hero}>
             <div className={styles.hero_left}>
                 <h1>
-                    International Conference on <span>Intelligent Systems</span> and<br />
-                    Industrial Instrumentation<br />(CISII-24)
+                    International Conference on <span>Intelligent Systems</span>
+                    and Industrial Instrumentation<br />
+                    (CISII-2024)<br />
+                      <span>20 and 21 September 2024</span>
                 </h1>
                 <div onClick={() => router.push('/registration')} >
                     <CustomBtn text='Register' />
