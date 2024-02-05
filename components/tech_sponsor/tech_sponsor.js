f1e81d926413bae98c77fa7c778d481c0757d7cc
Subh2101 <90567811+Subh2101@users.noreply.github.com>
2024-02-05 11:25:41 +0530

    Update tech_sponsor_WIE

--- components/tech_sponsor/tech_sponsor.js
+++ components/tech_sponsor/tech_sponsor.js
@@ -27,7 +27,11 @@ const TechSponsor = () => {
         <div className={styles.sponsor}>
             <h1>Technical Sponsors</h1>
             <div className={styles.members}>
-      <Image src={'csi-logo.png'} alt='CSI'
+     <Image src={'wielogo.jpg'} alt='IEEEWIE'
+                    width={screenSize.width / 7} height={30} 
+                />
+     <h3>      </h3>  
+    <Image src={'csi-logo.png'} alt='CSI'
                     width={screenSize.width / 7} height={30} 
                 />
      <h3>      </h3>
