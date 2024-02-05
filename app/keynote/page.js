04ac87df8c2bbe61e947489c8f72e87381599327
Subh2101 <90567811+Subh2101@users.noreply.github.com>
2024-02-05 11:59:02 +0530

    Add files via upload

---
+++ app/keynote/page.js
@@ -0,0 +1,21 @@
+import styles from './imp_dates.module.scss'
+
+const date_info = [
+    "Paper submission open: 01 February 2024",
+    "Paper Submission Deadline: 15 May 2024",
+    "Acceptance Notification: 15 August 2024",
+    "Early Bird Registration: 31 August 2024",
+    "Camera Ready Submission: 10 September 2024",
+    "Regular Registration Closes: 10 September 2024"
+]
+
+const Dates = () => {
+    return (
+        <div className={styles.dates}>
+            <h1>Mark Your Calendar: Important Dates</h1>
+            {date_info.map((date, index) => { return <p key={index}>{date}</p> })}
+        </div>
+    )
+}
+
+export default Dates
