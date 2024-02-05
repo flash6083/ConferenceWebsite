fea691634b64ac615f89f44bef4d1fff847118c5
Subh2101 <90567811+Subh2101@users.noreply.github.com>
2024-02-05 12:32:51 +0530

    Update page_Submission Open date

--- app/imp_dates/page.js
+++ app/imp_dates/page.js
@@ -1,7 +1,7 @@
 import styles from './imp_dates.module.scss'
 
 const date_info = [
-    "Paper submission open: 01 February 2024",
+    "Paper submission open: 15 February 2024",
     "Paper Submission Deadline: 15 May 2024",
     "Acceptance Notification: 15 August 2024",
     "Early Bird Registration: 31 August 2024",
